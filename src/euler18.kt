44ef04e0779b3b9ccefde2172bb351ae337ccf79
Adam Boucher <aboucher598@gmail.com>
2015-12-22 18:20:47 +0000

    Reformatted problems 18 and 67

--- src/euler18.kt
+++ src/euler18.kt
@@ -1,7 +1,9 @@
 import java.io.File
 
-fun euler18(): Int {
-    var lines = File("res/euler18.txt").readLines().map { it.split(' ').map { it.toInt() }.toArrayList() }.toArrayList()
+fun euler18() = maximumPathSum("res/euler18.txt")
+
+fun maximumPathSum(fileName: String): Int {
+    var lines = File(fileName).readLines().map { it.split(' ').map { it.toInt() }.toArrayList() }.toArrayList()
     for (line in lines.size-2 downTo 0) {
         for (i in 0..lines[line].size-1) {
             lines[line][i] += Math.max(lines[line+1][i], lines[line+1][i+1])
